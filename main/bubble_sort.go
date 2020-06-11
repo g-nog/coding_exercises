3ecfc5aa0eeb4fb639a2c4bfeeace3f1c33046fb
Guilherme Nogueira <nogueig@gmail.com>
2020-06-11 10:00:12 -0300

    adding bubble_sort.go

--- main/bubble_sort.go
+++ main/bubble_sort.go
@@ -1,1 +1,42 @@
 package main
+
+func main() {
+
+	BubbleSort([]int{8, 5, 2, 9, 5, 6, 3})
+
+}
+
+func BubbleSort(array []int) []int {
+
+	end := len(array) - 1
+
+	for x := 0; x < len(array)-1; x++ {
+
+		swaps := 0
+
+		for i := 0; i < end; i++ {
+			var aux int
+
+			if array[i] > array[i+1] {
+				aux = array[i]
+				array[i] = array[i+1]
+				array[i+1] = aux
+				swaps++
+			} else if array[i] < array[i+1] {
+				swaps++
+			}
+
+			if i == end-1 {
+				end--
+			}
+
+		}
+
+		if swaps == 0 {
+			break
+		}
+	}
+
+	// Write your code here.
+	return array
+}
